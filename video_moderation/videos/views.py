fb1895cf856b432621a627690c51972cf464f0a8
UdayMalviya <udaymalviya521@gmail.com>
2025-03-28 18:44:27 +0530

    video-moderation-system

---
+++ video_moderation/videos/views.py
@@ -0,0 +1,20 @@
+from .models import Video
+from .serializers import VideoSerilizer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+
+# Create your views here.
+class VideoUploadView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        serializer = VideoSerilizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Video uploaded successfully!", "data": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
